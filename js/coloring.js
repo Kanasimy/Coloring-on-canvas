c89ef0a8042d003d9320e1bf3c4b3c2cc8dae086
ouzich <ouzich@ac-sw.com>
2014-05-07 17:07:36 +0700

    homework

--- js/coloring.js
+++ js/coloring.js
@@ -1,3 +1,27 @@
 /**
  * Created by uzich on 05.05.14.
  */
+
+(function( $ ) {
+
+    // Create plugin jquery
+    jQuery.fn.coloring = function(options) {
+
+         var defaultOptions = {
+            width: 600,
+            height: 600,
+            canvasId: this
+        }
+
+        var o = $.extend(defaultOptions,options);
+
+        var ctx   = o.canvasId[0].getContext('2d');
+
+        o.canvasId.on('click', function(event) {
+            console.log(event)
+            }
+        );
+
+    };
+
+})(jQuery);
